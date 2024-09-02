679de86632da8a867b79f3872a4591b52bae2366
Forrest Glover <forrestglover@yahoo.com>
2024-09-01 17:06:42 -0700

    Touch ups

--- core/Core/src/main.cpp
+++ core/Core/src/main.cpp
@@ -6,11 +6,6 @@
 #include "ByteStream/ByteStreamInterface.h"
 #include "Display/GuiInterface.h"
 
-// Error callback function for GLFW
-static void glfw_error_callback(int error, const char* description) {
-  std::cerr << "GLFW Error " << error << ": " << description << std::endl;
-}
-
 int main(int, char**) {
   ByteStreamInterface sock_test(UdpConfig{.recv_ip = "127.0.0.1",
                                           .recv_port = 8080,
